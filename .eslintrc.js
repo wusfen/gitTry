b1c4eda1d45b65e585e4c96521c591cc72656163
wushufen <wushufen@sunlands.com>
2019-03-25 20:56:09 +0800

    .

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,7 +34,7 @@ module.exports = {
     // 多空格
     "no-multi-spaces": [1],
     // 操作符旁空格
-    "space-infix-ops": [1],
+    "space-infix-ops": [1], 
     // 尾空格
     "no-trailing-spaces": [1],
     // 多空行
